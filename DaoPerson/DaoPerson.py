e985c7603fc5392eed6c7fea8c12512bddf14bd1
Roberto R <roberto.riveros.padilla101@gmail.com>
2022-08-04 15:20:42 -0500

    Main.py added and classes cleaning

--- DaoPerson/DaoPerson.py
+++ DaoPerson/DaoPerson.py
@@ -1,6 +1,5 @@
 from Person.Person import Person
 from Pool_Cursor import PoolCursor
-from logger_base import log
 
 
 class DaoPerson(object):
@@ -40,42 +39,3 @@ class DaoPerson(object):
             attribute: tuple = (person.id_person,)
             cursor.execute(cls._DELETE, attribute)
             return cursor.rowcount
-
-
-option: str = ''
-while option != '5':
-    print(f'Menu'.center(50, '-'))
-    print(f'1.- Show all registers.')
-    print(f'2.- Insert a new person.')
-    print(f'3.- Update a person into the database.')
-    print(f'4.- Delete a person into the database.')
-    print(f'5.- Exit')
-    option = input('Select an available digit: ')
-    if option == '1':
-        peoplet: list = DaoPerson.select()
-        for persont in peoplet:
-            print(f'\n{persont}')
-    elif option == '2':
-        name: str = input('Insert the name: ')
-        lastname: str = input('Insert the lastname: ')
-        email: str = input('Insert the email: ')
-        print(f'Registers affedcted: {DaoPerson.insert(Person(name=name, lastname=lastname, email=email))}')
-    elif option == '3':
-        id_person: int = int(input('Insert the id person: '))
-        name: str = input('Insert the name: ')
-        lastname: str = input('Insert the lastname: ')
-        email: str = input('Insert the email: ')
-        print(f'Registers affedcted: {DaoPerson.update(Person(id_person, name, lastname, email))}')
-    elif option == '4':
-        id_person: int = int(input('Insert the id person: '))
-        print(f'Registers affedcted: {DaoPerson.delete(Person(id_person=id_person))}')
-    else:
-        print('Exit')
-
-
-# alice = Person(name='Mary', lastname='Smith', email='alicieGe@gmail.com')
-# sandra = Person(9, 'Sandra', 'Medardo', 'sandraMe@gmail.com')
-# print(DaoPerson.update(sandra))
-# peoplet = DaoPerson.select()
-# for persont in peoplet:
-#     log.debug(f'\n{persont}')
